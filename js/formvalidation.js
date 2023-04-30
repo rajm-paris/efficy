4d40a78d1a81b51111c6d47069659ab9e55367ad
Raj <rajm.paris@gmail.com>
2023-04-30 22:01:38 +0200

    Select populating and Form Submission

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -28,12 +28,60 @@ const oppoStatus = [
 
 const FormComponent = class {
   constructor() {
+    this.populateSelect();//populate Select options
   }
   start() {
-    // Start modifying the form elements here!
-    // You are allowed to add extra methods, properties or change the constructor of this class
+    this.handleForm();//Form Submit
   }
-}
+
+  /*
+  ** Populate the Select options with given Data
+  */
+  populateSelect() {
+    const select = document.querySelector('select[name="status"]');
+    if (!select) {
+        console.error('Select element not found.');
+        return false;
+    }
+    oppoStatus.forEach(({ K_OPPO_STATUS, SUCCESS, STATUS }) => {
+        const option = new Option(STATUS, K_OPPO_STATUS);
+        option.dataset.success = SUCCESS;
+        select.add(option);
+    });
+  }//Eof Method(onFormSubmit)
+
+  /*
+	** Add dynamic Form Submission Event 
+	*/
+	handleForm(){
+		const form =  document.querySelector('form') || document.getElementsByTagName('form')[0];
+    if (!form) {
+        console.error('Form element not found.');
+        return false;
+    }
+
+		form.addEventListener("submit", FormComponent.onFormSubmit);//attaching submition event		
+	}//Eof Method(handleForm)
+	
+	/*
+	** Form Manipulations
+	** Static so that in future can be used  without invoking/creating object
+	*/
+	static onFormSubmit(event) {
+		event = event || window.event;
+		event.preventDefault();
+		
+		const data 			= new FormData(event.target);
+		const dataObject 	= Object.fromEntries(data.entries());
+		const output 		= document.getElementsByClassName("output")[0];
+		const output_text	= document.createElement('p');
+		
+		output_text.innerText = JSON.stringify(dataObject);
+		output.textContent = '';//will be faster than innerHTML as browsers won't invoke their HTML parsers 
+		output.appendChild(output_text);
+	}//Eof Method(onFormSubmit)
+
+}//Eof class(FormComponent)
 
 const fc = new FormComponent();
 fc.start();
